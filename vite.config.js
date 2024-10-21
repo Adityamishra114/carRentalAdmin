34495adefbc3f8c734694f47980ce23b748acdbe
Aditya Kumar Mishra <89987516+Adityamishra114@users.noreply.github.com>
2024-10-21 13:49:36 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+dotenv.config(); 
 
 const isProduction = import.meta.env.VITE_APP_PROD_ENV === 'production';
 
