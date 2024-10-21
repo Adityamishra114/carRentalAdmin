cd793e6f13ffcb2f2c0a0dceb8dd9d40b13669aa
Aditya Kumar Mishra <89987516+Adityamishra114@users.noreply.github.com>
2024-10-21 12:12:05 +0530

    newChanges

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom/client";
-import App from "../../src/App.jsx";
 import "./index.css";
 import { BrowserRouter  } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter  >
+  <BrowserRouter   >
     <App />
   </BrowserRouter>
 );
