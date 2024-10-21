aa04aa218382e9505990e221a373480dc894f43d
Aditya Kumar Mishra <89987516+Adityamishra114@users.noreply.github.com>
2024-10-21 12:41:40 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-
-dotenv.config();
-
 
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.VITE_BASE_URL || '/',
+  base: '/',
 });
