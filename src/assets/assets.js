5e40f162a75673be5f74a7e931a0402c48ff1d41
Aditya Kumar Mishra <89987516+Adityamishra114@users.noreply.github.com>
2024-10-21 11:58:15 +0530

    addall

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,10 +1,10 @@
-import logo from './logo.png'
-import add_icon from './add_icon.png'
-import order_icon from './order_icon.png'
-import profile_image from './profile_image.png'
-import upload_area from './upload_area.png'
-import parcel_icon from './parcel_icon.png'
-import video from "./video123.mp4"
+import logo from '../../public/assets/add_icon.png'
+import add_icon from '../../public/assets/add_icon.png'
+import order_icon from '../../public/assets/order_icon.png'
+import profile_image from '../../public/assets/profile_image.png'
+import upload_area from '../../public/assets/upload_area.png'
+import parcel_icon from '../../public/assets/parcel_icon.png'
+import video from "../../public/assets/video123.mp4"
 
 export const assets ={
     logo,
