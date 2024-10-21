6b1db301073b600377d8c5192ee53ec6804087fe
Aditya Kumar Mishra <89987516+Adityamishra114@users.noreply.github.com>
2024-10-21 14:13:58 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-
-dotenv.config(); 
-
-const isProduction = import.meta.env.VITE_APP_PROD_ENV === 'production';
 
 export default defineConfig({
-  base: isProduction ? '/carRentalAdmin/' : '/',
   plugins: [react()],
+  base: "/",
 });
